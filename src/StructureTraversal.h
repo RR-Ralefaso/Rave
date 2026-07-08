c5e86ee205951d0be78133ac2e4df6d1483c661c
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-08 13:41:10 +0200

    header file for traversing

---
+++ src/StructureTraversal.h
@@ -0,0 +1,18 @@
+#ifndef STRUCUTURETRAVERSAL_H
+#define STRUCUTURETRAVERSAL_H
+#include "structures.h"
+
+
+
+
+
+
+
+
+
+// --prototype functions--
+void TraversePreOrder(TreeNode *root);
+void TraverseInOrder(TreeNode *root);
+void TraversePostOrder(TreeNode *root);
+
+#endif
