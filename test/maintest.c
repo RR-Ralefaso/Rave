4ddf77d49c74c0b086dec7e998fff9a51576691c
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-09 23:22:05 +0200

    update

--- test/maintest.c
+++ test/maintest.c
@@ -1,26 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h> 
 #include "../src/structures.h"
 #include "../src/StructureTraversal.h"
 
-
-
-
 int main(int argc, char const *argv[])
 {
-    TreeNode *root = Init(InitStack(0),"master"); //inititating
-    //trying to find a branch that doesnt exist
-    TreeNode *childNE = Init(InitStack(0), "child"); //child that doesnt exist
-    Find(root,"child"); // should return false as branch does not exist
+    // clock_t for standard tick tracking
+    clock_t start, end;
 
-    //checking if traversing works
+    start = clock();
+
+    // ---CODE ---
+    TreeNode *root = Init(InitStack(0), "master");
+
+    CreateChild(root, "");
+
+    printf("--- Traversal Output ---\n");
     TraverseInOrder(root);
+    printf("------------------------\n");
 
-
-    //cleaning up
     free(root->data->arr);
     free(root->data);
     free(root);
+    // -------------------------
+
+    end = clock();
+
+    // Calculate time spent in microseconds
+    double microseconds = ((double)(end - start) / CLOCKS_PER_SEC) * 1e6;
+
+    printf("\nExecution Speed: %.2f microseconds (us)\n", microseconds);
+
     return 0;
 }
-
