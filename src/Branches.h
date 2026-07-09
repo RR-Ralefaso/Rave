2a6d672529bbfd5b75cfeaf1e9f7b7e1608fb9b5
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-09 23:43:48 +0200

    header file for branches.c

---
+++ src/Branches.h
@@ -0,0 +1,16 @@
+#ifndef BRANCHES_H
+#define BRANCHES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "structures.h"
+
+
+
+
+//--prototype functions
+
+// adding a childnode
+TreeNode *CreateChild(TreeNode *root, char *childname);
+
+#endif
