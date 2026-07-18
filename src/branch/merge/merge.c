6a0a1f7bc119ab5928d2721ac41c275b0673b204
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-18 23:45:22 +0200

    merging is basically commiting so i just commited -> change this in the future

--- src/branch/merge/merge.c
+++ src/branch/merge/merge.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "merge.h"
 #include "../../structures/structures.h"
+#include "../../commits/commits.h"
 #include "../../structures/structure_traversal/StructureTraversal.h"
 
 //merging should merge branches
@@ -35,25 +36,5 @@ void MergeBranches(TreeNode *branchMT, TreeNode *branchBM)
 
     // i was supposed to make sure it merges onto it as a commit there for the commit becomes the merge inside the node alowinf or data of that
     // commit to become the merge of the two branches (it shouldnt form a new branch)
-
-    //the new capcity for the merge
-    int capacity = branchMT->data->capacity + branchBM->data->capacity+1;
-
-    // the branch stacks of each branch 
-    BranchStack
-        *stack_branchMT = branchMT->data,
-        *stack_branchBM = branchBM->data;
-
-    // Expands the existing block of contiguous memory to fit the new capacity
-    Node *temp_arr = (Node *)realloc(stack_branchMT, capacity * sizeof(Node));
-
-    if(temp_arr==NULL)
-    {
-        perror("Error : failed to reallocate memory to grow stack\n");
-        return;
-    }
-
-    stack_branchMT->arr = temp_arr;
-    
-
+    Commit(branchMT, branchBM);
 }
