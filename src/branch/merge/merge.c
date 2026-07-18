0721e26305f322535e2e820385f49b20c88ea500
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-18 15:38:41 +0200

    updated function merge > gotta figure out how to merge data

--- src/branch/merge/merge.c
+++ src/branch/merge/merge.c
@@ -7,6 +7,9 @@
 //merging should merge branches
 void MergeBranches(TreeNode *branchMT, TreeNode *branchBM)
 {
+
+    //MT -> merged to
+    //BM -> Being merged
     // Ensure we do not attempt to merge NULL tree nodes
     if (branchMT == NULL || branchBM == NULL)
     {
@@ -20,5 +23,37 @@ void MergeBranches(TreeNode *branchMT, TreeNode *branchBM)
         fprintf(stderr, "Error: Cant merge empty BranchStacks\n");
         return;
     }
+
+
+    //ensurnig that there is data to merge
+    if (branchMT->data->arr->data==NULL || branchBM->data->arr->data==NULL)
+    {
+        fprintf(stderr, "Error : Cant merge empty data nodes\n");
+        return;
+    }
+
+
+    // i was supposed to make sure it merges onto it as a commit there for the commit becomes the merge inside the node alowinf or data of that
+    // commit to become the merge of the two branches (it shouldnt form a new branch)
+
+    //the new capcity for the merge
+    int capacity = branchMT->data->capacity + branchBM->data->capacity+1;
+
+    // the branch stacks of each branch 
+    BranchStack
+        *stack_branchMT = branchMT->data,
+        *stack_branchBM = branchBM->data;
+
+    // Expands the existing block of contiguous memory to fit the new capacity
+    Node *temp_arr = (Node *)realloc(stack_branchMT, capacity * sizeof(Node));
+
+    if(temp_arr==NULL)
+    {
+        perror("Error : failed to reallocate memory to grow stack\n");
+        return;
+    }
+
+    stack_branchMT->arr = temp_arr;
+    
 
 }
