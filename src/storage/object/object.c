5813b6dde5d4a382a49f7b7dd8b7cc70595cfe1c
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-27 21:42:33 +0200

    initialised objects

--- src/storage/object/object.c
+++ src/storage/object/object.c
@@ -3,4 +3,15 @@
 #include <sys/types.h>
 #include "object.h"
 
+//Writing the objects 
+void WriteObject(const unsigned char *data , size_t len , const char *type)
+{
 
+}
+
+
+//reading the object
+void ReadObject(const char *hash)
+{
+    
+}
