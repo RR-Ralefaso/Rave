ef4a9a6efdf0959ee96bf9b642466c29504e1e81
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-30 01:32:20 +0200

    updated prototype functions

--- src/checker/checks.h
+++ src/checker/checks.h
@@ -1,13 +1,26 @@
 #ifndef CHECKS_H
 #define CHECKS_H
 
-#include <stdlib.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
 
 //--Protype functions
+
+// checks if file is open on windows
+int IsFileOpen_WIN(const char *filepath);
+
+// checks if file is open on macos
+int IsFileOpen_macOS(const char *filepath);
+
+// checks if file is open on linux
+int IsFileOpen_Linux(const char *filepath);
+
+// cross platform wrapper to check if file is open
 int IsFileOpen(const char *filepath);
+// checks if a file ends with a specific extension -- helper function
 int HasExtension(const char *filename, const char *ext);
+
+// scans the directory recursively
 void ScanProjectDir(const char *dir_path);
 
 #endif
