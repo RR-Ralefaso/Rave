7fa90d4e8af5014ca4715247b5ae51ffcf4ba638
RR-Ralefaso <rrralefaso@outlook.com>
2026-07-30 01:27:12 +0200

    need to use a function i havent created yet

--- src/storage/object/object.c
+++ src/storage/object/object.c
@@ -1,20 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include "object.h"
+#include "../storing.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
-//Writing the objects 
+// Writing the objects
 
 // writes objects into the directory .rave/objects
-void WriteObject(const unsigned char *data , size_t len , const char *type)
-{
-    
+void WriteObject(const unsigned char *data, size_t len, const char *type) {
+  // error checkig first
+  if (data == NULL || type == NULL) {
+    fprintf(stderr, "Error : data and or type are NUll\n");
+    return;
+  }
 
+  /*
+      TODO : use a function called DirExists to see if the directory exists
+  */
 }
 
-
-//reading the object
-void ReadObject(const char *hash)
-{
-    // TODO : create a way of reading objects
+// reading the object
+void ReadObject(const char *hash) {
+  // TODO : create a way of reading objects
 }
